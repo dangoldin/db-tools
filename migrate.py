3991d0d1e7ea51ea0ad2365215593c74a9ed3781
Dan Goldin <dangoldin@gmail.com>
2017-02-15 22:38:43 -0500

    A little bit more progress

--- migrate.py
+++ migrate.py
@@ -12,6 +12,7 @@ class Migrator:
             user=config.USER,
             passwd=config.PASSWORD,
             db=config.DB)
+        self.parse_map()
 
     def __del__(self):
         if self.db:
@@ -19,6 +20,16 @@ class Migrator:
                 self.db.close()
             except Exception, e:
                 pass
+
+    def parse_map(self):
+        # Go from table1.field1 => table2.field2
+        # to table1 = {field1 => table2.field2}
+        self.table_map_v2 = {}
+        for k1, k2 in self.table_map.iteritems():
+            t1, f1 = k1.split('.')
+            if t1 not in self.table_map_v2:
+                self.table_map_v2[t1] = {}
+            self.table_map_v2[t1][f1] = k2
 
     def run_select(self, q):
         cur = self.db.cursor()
@@ -36,30 +47,32 @@ class Migrator:
     def run_insert(self, table, rows, key):
         cur = self.db.cursor()
         for row in rows:
-            # Wipe data before hand
-            self.run_delete(table, key, row[key])
             # Insert new data
             vals = ','.join('"{0}"'.format(str(x)) for x in row.values())
+            # Hack for now to deal with nulls
+            vals = vals.replace('"None"', 'NULL')
             q = 'INSERT INTO {0} ({1}) VALUES ({2})'.format(table, ','.join(row.keys()), vals)
             # cur.execute(q)
             print q
 
     def run_migration(self, start_key, start_val):
-        if start_key not in self.table_map:
-            print 'Unable to find map info for ' + start_key + '.. stoping'
-            return
-
         table, field = start_key.split('.')
 
         rows = self.run_select('SELECT * from {0} where {1} = "{2}"'.format(table, field, str(start_val)))
 
         vals = [row[field] for row in rows]
+        # Dedup
+        vals = list(set(vals))
+        for val in vals:
+            self.run_delete(table, field, val)
 
         self.run_insert(table, rows, field)
 
-        next_key = self.table_map[start_key]
-        for val in vals:
-            self.run_migration(next_key, val)
+        if table in self.table_map_v2:
+            next_steps = self.table_map_v2[table]
+            for field, key in next_steps.iteritems():
+                for val in vals:
+                    self.run_migration(key, val)
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
