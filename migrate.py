457804558b95961d7e6ede9fc1c07b161f240aaf
Dan Goldin <dangoldin@gmail.com>
2017-02-17 17:52:55 -0500

    Got it working pretty well

--- migrate.py
+++ migrate.py
@@ -1,29 +1,56 @@
 #! /usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Simple script to migrate data from one database to another given
+a starting point
+"""
 
 import sys
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 import MySQLdb
 import config
 
 class Migrator:
-    def __init__(self, table_map):
+    """ Class to help migrate data from one set of tabls to another
+    """
+    def __init__(self, table_map, dry_run = True):
         self.table_map = table_map
-        self.db = MySQLdb.connect(
-            host=config.HOST,
-            user=config.USER,
-            passwd=config.PASSWORD,
-            db=config.DB)
+        self.read_db = MySQLdb.connect(
+            host=config.READ_HOST,
+            user=config.READ_USER,
+            passwd=config.READ_PASSWORD,
+            db=config.READ_DB)
+        self.write_db = MySQLdb.connect(
+            host=config.WRITE_HOST,
+            user=config.WRITE_USER,
+            passwd=config.WRITE_PASSWORD,
+            db=config.WRITE_DB)
         self.parse_map()
+        self.dry_run = dry_run
+        self.read_cursor = self.read_db.cursor()
+        self.write_cursor = self.write_db.cursor()
 
     def __del__(self):
-        if self.db:
+        if self.read_db:
             try:
-                self.db.close()
+                self.read_db.close()
+            except Exception, e:
+                pass
+        if self.write_db:
+            try:
+                self.write_db.commit()
+                self.write_db.close()
             except Exception, e:
                 pass
 
     def parse_map(self):
-        # Go from table1.field1 => table2.field2
-        # to table1 = {field1 => table2.field2}
+        """
+        Go from table1.field1 => table2.field2
+        to table1 = {field1 => table2.field2}
+        """
         self.table_map_v2 = {}
         for k1, k2 in self.table_map.iteritems():
             t1, f1 = k1.split('.')
@@ -31,38 +58,48 @@ class Migrator:
                 self.table_map_v2[t1] = {}
             self.table_map_v2[t1][f1] = k2
 
-    def run_select(self, q):
-        cur = self.db.cursor()
-        cur.execute(q)
-        print q
+    def run_select(self, q, val):
+        logging.debug('Select query: ' + q)
+        cur = self.read_cursor
+        cur.execute(q, (str(val), ))
         fields = map(lambda x:x[0], cur.description)
         return [dict(zip(fields, row)) for row in cur.fetchall()]
 
     def run_delete(self, table, key, val):
-        cur = self.db.cursor()
-        q = 'DELETE FROM {0} WHERE {1} = "{2}"'.format(table, key, val)
-        # cur.execute(q)
-        print q
+        q = 'DELETE FROM {0} WHERE {1} = %s'.format(table, key)
+        logging.debug('Delete query: ' + q)
+        cur = self.write_cursor
+        if not self.dry_run:
+            cur.execute(q, (str(val), ))
 
     def run_insert(self, table, rows, key):
-        cur = self.db.cursor()
+        if not rows:
+            return
+        cur = self.write_cursor
+        fields = rows[0].keys()
+        num_fields = len(fields)
+
+        q = 'INSERT INTO {0} ({1}) VALUES ({2})'.format(table, ','.join(fields), '%s,' * (num_fields-1) + '%s')
         for row in rows:
-            # Insert new data
-            vals = ','.join('"{0}"'.format(str(x)) for x in row.values())
-            # Hack for now to deal with nulls
-            vals = vals.replace('"None"', 'NULL')
-            q = 'INSERT INTO {0} ({1}) VALUES ({2})'.format(table, ','.join(row.keys()), vals)
-            # cur.execute(q)
-            print q
+            logging.debug('Insert query: ' + q)
+            if not self.dry_run:
+                cur.execute(q, row.values())
 
     def run_migration(self, start_key, start_val):
         table, field = start_key.split('.')
 
-        rows = self.run_select('SELECT * from {0} where {1} = "{2}"'.format(table, field, str(start_val)))
+        rows = self.run_select('SELECT * from {0} where {1} = %s'.format(table, field), start_val)
 
         vals = list(set([row[field] for row in rows]))
         for val in vals:
             self.run_delete(table, field, val)
+
+        # Also delete the primary keys to avoid primary key conflicts
+        # For now just do 'id' but we should do more introspection
+        if len(rows) and 'id' in rows[0].keys():
+            ids = list(set([row['id'] for row in rows]))
+            for i in ids:
+                self.run_delete(table, 'id', val)
 
         self.run_insert(table, rows, field)
 
@@ -76,10 +113,9 @@ class Migrator:
 if __name__ == '__main__':
     if len(sys.argv) != 3:
         raise Exception('Please specify start table.key and id')
-        exit(1)
 
-    start_key = sys.argv[1]
-    start_val = sys.argv[2]
+    START_KEY = sys.argv[1]
+    START_VAL = sys.argv[2]
 
-    m = Migrator(config.TABLE_MAP)
-    m.run_migration(start_key, start_val)
+    MIGRATOR = Migrator(config.TABLE_MAP, False)
+    MIGRATOR.run_migration(START_KEY, START_VAL)
